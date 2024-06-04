50d012defb776f63ef37a2fbfd4c31245727fdf3
williamvdm <williamvdm@live.nl>
2024-06-04 17:06:13 +0200

    buttonzz

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,17 @@
-import ArrowUp from "../assets/arrow-up.svg";
+// Assets
+import ArrowUp from "../assets/arrow-up.svg"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 relative bg-illusion">
-      <div className="absolute bottom-0 w-full flex justify-center mb-8">
+      <div className="absolute bottom-0 flex justify-center mb-8 bg-white p-4 w-1/2 rounded-full">
         <input
           type="text"
-          className="rounded-full p-3 w-1/2 text-wepeep outline-none relative pr-10"
+          className="text-wepeep outline-none relative pr-10 w-full"
           placeholder="Type a message..."
         />
-        <button type="button">
-          <img src={ArrowUp}/>
+        <button type="button" className="">
+          <img src="https://i.ibb.co/b65pWGc/arrow-up.png" className="size-11 mx-4"/>
         </button>
       </div>
     </main>
