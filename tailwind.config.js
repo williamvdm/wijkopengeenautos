8399416d576f3f9cb06eb6f0e1844ac5be7bc5ee
williamvdm <williamvdm@live.nl>
2024-06-04 16:21:11 +0200

    pinkk

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,11 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        illusion:'#FFEBF0',
+        wepeep:'#F692AE',
+        remy:'#FDE2ED',
+        havelockblue:'#67A3DA',
       },
     },
   },
