7609eae618170d32042232404277faf1ddb1acd8
williamvdm <williamvdm@live.nl>
2024-06-04 22:42:06 +0200

    update:responsive input field

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,16 @@
-// Assets
-import ArrowUp from "../assets/arrow-up.svg"
+import ArrowUp from "../assets/arrow-up.svg";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 relative bg-illusion">
-      <div className="absolute bottom-0 flex justify-center mb-8 bg-white p-4 w-1/2 rounded-full">
+      <div className="absolute bottom-0 inset-x-4 flex justify-center mx-auto mb-8 bg-white p-4 rounded-full max-w-3xl">
         <input
           type="text"
-          className="text-wepeep outline-none relative pr-10 w-full"
+          className="text-wepeep outline-none relative p-2 w-full"
           placeholder="Type a message..."
         />
-        <button type="button" className="">
-          <img src="https://i.ibb.co/b65pWGc/arrow-up.png" className="size-11 mx-4"/>
+        <button type="button" className="w-11 mx-4">
+          <img src="https://i.ibb.co/b65pWGc/arrow-up.png" className="w-full"/>
         </button>
       </div>
     </main>
